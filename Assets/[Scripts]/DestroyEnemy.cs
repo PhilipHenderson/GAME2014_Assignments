883645ed1c915f0237350d6ab45cd616c285f0bb
Philip Henderson <philip.henderson@hotmail.com>
2022-10-21 00:13:07 -0400

    Added Top and Bottom UI (Non Functional)

--- Assets/[Scripts]/DestroyEnemy.cs
+++ Assets/[Scripts]/DestroyEnemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyEnemy : MonoBehaviour
 {
-    public void OnCollisionEnter2D(other)
+    public void OnCollisionEnter2D(Collision2D other)
     {
         Destroy(other.gameObject);  
     }
