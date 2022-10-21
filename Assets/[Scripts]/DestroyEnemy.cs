d00df36ec9020013f733e3f8d767ff94879f33f1
Philip Henderson <philip.henderson@hotmail.com>
2022-10-20 23:43:08 -0400

    Added Top Ui Elements, Created Instructions and Gameover Screens

--- Assets/[Scripts]/DestroyEnemy.cs
+++ Assets/[Scripts]/DestroyEnemy.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class DestroyEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnCollisionEnter2D(other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(other.gameObject);  
     }
 }
